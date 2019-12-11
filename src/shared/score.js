ad815f75520a2b5856ed836e392bd6dfffd6de46
belljonathan50 <belljonathan50@gmail.com>
2019-12-11 11:48:57 +0100

    Update score.js

--- src/shared/score.js
+++ src/shared/score.js
@@ -1,73 +1,65 @@
 // score definition
 const score = {
-  title: 'Šîr HaŠîrîm - Hoheslied',
-  duration: 19 * 60, 
+  title: 'Common Grounds',
+  duration: 14 * 60, // ...
   parts: {
     // links
 
-    'soprano1': {
-      type: 'performer',
-      file: 'videos/soprano1.mp4',
-    },
-    'soprano2': {
-      type: 'performer',
-      file: 'videos/soprano2.mp4',
-    },
-    'soprano3': {
-      type: 'performer',
-      file: 'videos/soprano3.mp4',
-    },
-    'alto1': {
-      type: 'performer',
-      file: 'videos/alto1.mp4',
-    },
-    'alto2': {
-      type: 'performer',
-      file: 'videos/alto2.mp4',
-    },
-    'alto3': {
-      type: 'performer',
-      file: 'videos/alto3.mp4',
-    },
-    'tenor1': {
-      type: 'performer',
-      file: 'videos/tenor1.mp4',
-    },
-    'tenor2': {
-      type: 'performer',
-      file: 'videos/tenor2.mp4',
-    },
-    'tenor3': {
-      type: 'performer',
-      file: 'videos/tenor3.mp4',
-    },
-    'bass1': {
-      type: 'performer',
-      file: 'videos/basse1.mp4',
-    },
-    'bass2': {
-      type: 'performer',
-      file: 'videos/basse2.mp4',
-    },
-    'bass3': {
-      type: 'performer',
-      file: 'videos/basse3.mp4',
-    },
-    'fullscore and tape': {
+    'Feldman-voice1': {
       type: 'env',
-      file: 'videos/fullscore and tape.mp4',
+      file: 'videos/feldmanestelle.mp4',
     },
-    'spectrogram': {
+    'Feldman-voice2': {
       type: 'env',
-      file: 'videos/spectrogram.mp4',
+      file: 'videos/feldmanlaurence.mp4',
     },
-    'tape12': {
+    'Feldman-voice3': {
       type: 'env',
-      file: 'videos/tape12.mp4',
+      file: 'videos/feldmaneugenie.mp4',
     },
-    'tape34': {
+    'Hope-voice1': {
       type: 'env',
-      file: 'videos/tape34.mp4',
+      file: 'videos/Hope-voice1.mp4',
+    },
+    'Hope-voice2': {
+      type: 'env',
+      file: 'videos/Hope-voice2.mp4',
+    },
+    'Hope-voice3': {
+      type: 'env',
+      file: 'videos/Hope-voice3.mp4',
+    },
+   'Hope-fullscore': {
+      type: 'env',
+      file: 'videos/Hope-fullscore.mp4',
+    },
+    'Bell-solo(Judith)': {
+      type: 'env',
+      file: 'videos/Bell-solo(Judith).mp4',
+    },
+    'Bell-voice1': {
+      type: 'env',
+      file: 'videos/Bell-voice1.mp4',
+    },
+    'Bell-voice2': {
+      type: 'env',
+      file: 'videos/Bell-voice2.mp4',
+    },
+    'Bell-voice3': {
+      type: 'env',
+      file: 'videos/Bell-voice3.mp4',
+    },
+    'Bell-video1': {
+      type: 'env',
+      file: 'videos/Bell-video1.mp4',
+    },
+    'Bell-video2': {
+      type: 'env',
+      file: 'videos/Bell-video2.mp4',
+    },
+    'Bell-fullscore': {
+      type: 'env',
+      file: 'videos/Bell-fullscore.mp4',
     },
     // env
   },
@@ -75,109 +67,280 @@ const score = {
 
     alpha: {
       time: 0,
-      label: "beginning",
+      label: "ivo piangendo",
     },
     qalpha: {
-      time: 14,
-      label: "sir-ha sop.3",
+      time: 59,
+      label: "je quitte donc le monde",
     },
     beta: {
-      time: 28,
-      label: "spectral arpegio",
+      time: 185,
+      label: "ivo piangendo - imitations",
     },
    bwdeta: {
-      time: 46,
-      label: "lvi slomoh",
+      time: 225,
+      label: "cosa mortale",
     },
     gamma: {
-      time: 71,
-      label: "slomoh - FM synthesis",
+      time: 315,
+      label: "Ivo,par groupes",
     },
     delta: {
-      time: 94,
-      label: "yssaqeni",
-    },
-      epsi: {
-      time: 127,
-      label: "minnesi ts ts ts",
-    },
-      kepsi: {
-      time: 145,
-      label: "shlomo",
-    },
-    epsilon: {
-      time: 220,
-      label: "shwora-wenara",
-    },
-    dzeta: {
-      time: 254,
-      label: "lereha",
-    },
-    eta: {
-      time: 308,
-      label: "habi'ani",
-    },
-    theta: {
-      time: 355,
-      label: "nazkira",
-    },
-    aiota: {
-      time: 413,
-      label: "nagila",
+      time: 360,
+      label: "cosa mortale",
     },
 
-      qepsi: {
-      time: 453,
-      label: "semene - glissandi",
+      epsi: {
+      time: 383,
+      label: "fin",
     },
-      qkepsi: {
-      time: 500,
-      label: "moshkheni ahareykha",
-    },
-    qepsilon: {
-      time: 517,
-      label: "ahebukha",
-    },
-    qdzeta: {
-      time: 641,
-      label: "hebiani",
-    },
-    qeta: {
-      time: 694,
-      label: "shrora",
-    },
-    qtheta: {
-      time: 746,
-      label: "hammelekh",
-    },
-    qaiota: {
-      time: 778,
-      label: "qedar",
-    },
-      wepsi: {
-      time: 822,
-      label: "ke'âole",
-    },
-      wkepsi: {
-      time: 863,
-      label: "moshreni - unisson",
-    },
-    wepsilon: {
-      time: 899,
-      label: "wenara",
-    },
-    wdzeta: {
-      time: 942,
-      label: "mesarim",
-    },
-    weta: {
-      time: 1011,
-      label: "kiriot",
-    },
-    wtheta: {
-      time: 1087,
-      label: "nazkira",
-    },
+   
+
+
+
+  //   alpha: {
+  //     time: 0,
+  //     label: 'Commencement',
+  //   },
+
+  //   qalpha: {
+  //     time: 17,
+  //     label: 'DEBUT nuage',
+  //   },
+
+
+  //   beta: {
+  //     time: 57,
+  //     label: 'Estelle',
+  //   },
+
+
+  //  bwdeta: {
+  //     time: 64,
+  //     label: 'PRENDRE LE TEMPS DES NUAGES puis nappes...',
+  //   },
+
+  //   gamma: {
+  //     time: 76,
+  //     label: 'tenues',
+  //   },
+  //   delta: {
+  //     time: 101,
+  //     label: 'm\'abondonner a leur mousse',
+  //   },
+
+  //     epsi: {
+  //     time: 112,
+  //     label: 'SKIP',
+  //   },
+  //         kepsi: {
+  //     time: 202,
+  //     label: 'LE MONDE EN RESTANT DANS CE MONDE',
+  //   },
+  //   epsilon: {
+  //     time: 129,
+  //     label: 'prendre le temps des nuages',
+  //   },
+  //   dzeta: {
+  //     time: 161,
+  //     label: 'reprise',
+  //   },
+  //   eta: {
+  //     time: 199,
+  //     label: 'je quitte donc le monde',
+  //   },
+
+
+
+  //   theta: {
+  //     time: 225,
+  //     label: 'JE RECOURS AUX FORETS',
+  //   },
+
+
+
+
+
+
+  //   iota: {
+  //     time: 236,
+  //     label: 'je veux entendre le vent',
+  //   },
+  //   kappa: {
+  //     time: 248,
+  //     label: 'la fenêtre ouverte en été',
+  //   },
+  //  lamdba: {
+  //     time: 268,
+  //     label: 'je veux entendre le vent dans les tilleuls',
+  //   },
+
+  //       preeta: {
+  //     time: 272,
+  //     label: 'ENTEN-EN-DRE LE VENTS glissando - OUVERTE EN ETE',
+  //   },
+
+  //  mu: {
+  //     time: 284,
+  //     label: 'la fenêtre ouverte en été 2',
+  //   },
+  //  nu: {
+  //     time: 293,
+  //     label: 'sentir le parfum séminal de ses fleurs',
+  //   },
+  //  MI: {
+  //     time: 307,
+  //     label: 'ACCELERATION',
+  //   },
+
+  // MqwI: {
+  //     time: 325,
+  //     label: 'LA LUMIERE UNISSON',
+  //   },
+
+
+
+  // MweI: {
+  //     time: 338,
+  //     label: 'IMITATIONS QUI MONTENT',
+  //   },
+
+
+
+  //  Zeri: {
+  //     time: 368,
+  //     label: 'JE VEUX SENTIR LE SOLEIL',
+  //   },
+
+  //  a: {
+  //     time: 380,
+  //     label: 'la lumiere dans mon ame',
+  //   },
+  //  b: {
+  //     time: 397,
+  //     label: 'la pluie et l\'ondée',
+  //   },
+  //  c: {
+  //     time: 416,
+  //     label: ' LUMIERE',
+  //   },
+
+
+  //  cr: {
+  //     time: 416,
+  //     label: 'SON ODEUR APRES L\'ORAGE puis LUMIERE',
+  //   },
+
+  //  cqwe: {
+  //     time: 425,
+  //     label: 'son parfum de tombe',
+  //   },
+
+  //  dqwe: {
+  //     time: 451,
+  //     label: 'je veux la brise et la bise',
+  //   },
+
+  //  e: {
+  //     time: 462,
+  //     label: 'BRI-ISE ET LA BI-ISE',
+  //   },
+
+  // sdfe: {
+  //     time: 502,
+  //     label: 'son parfum de tombe GLISSANDO puis rien',
+  //   },
+
+
+  //  ewe: {
+  //     time: 512,
+  //     label: 'son parfum de tombe 2',
+  //   },
+
+  //  fasd: {
+  //     time: 552,
+  //     label: 'JE VEUX JE VEUX',
+  //   },
+
+  //  f: {
+  //     time: 558,
+  //     label: 'JE VEUX JE VEUX imitations',
+  //   },
+
+
+
+  //  g: {
+  //     time: 582,
+  //     label: 'ÉLARGI',
+  //   },
+
+  //  hdcdc: {
+  //     time: 594,
+  //     label: 'son parfum de tombe 3',
+  //   },
+
+  //  gdsf: {
+  //     time: 602,
+  //     label: 'DE TOMBE A VENIR',
+  //   },
+
+  //  geddsf: {
+  //  time: 616,
+  //     label: 'JE VEUX RETROUVER LE GOUT',
+  //   },
+
+  //  geefgddsf: {
+  //  time: 648,
+  //     label: 'ECRASER DES FRAISES',
+  //   },
+
+
+
+  // gdqwe: {
+  //  time: 697,
+  //     label: 'RESPIRER A FLEUR',
+  //   },
+
+  //  l: {
+  //     time: 701,
+  //     label: 'respirer la fleur',
+  //   },
+  //  m: {
+  //     time: 735,
+  //     label: 'devenir moi-même pomme',
+  //   },
+  //  n: {
+  //     time: 758,
+  //     label: 'm\'allonger dans l\'herbe',
+  //   },
+  //  o: {
+  //     time: 782,
+  //     label: 'm\'endormir le dos épousant la terre',
+  //   },
+  //  p: {
+  //     time: 833,
+  //     label: 'JE VEUX VOIR DES COULEUVRES',
+  //   },
+  //  q: {
+  //     time: 864,
+  //     label: 'Je veux voir l\'orvet',
+  //   },
+  //  r: {
+  //     time: 885,
+  //     label: 'chhhhh',
+  //   },
+  //  s: {
+  //     time: 907,
+  //     label: 'qui ondule sans queue ni tête',
+  //   },
+  //  t: {
+  //     time: 952,
+  //     label: 'JE VEUX ENTENDRE L\'EAU',
+  //   },
+  //  u: {
+  //     time: 987,
+  //     label: 'la pluie trouant la peau',
+  //   },
   },
 };
 
